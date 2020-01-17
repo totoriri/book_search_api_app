841d4260b15b75835e81a16c667dda03ebbfdb1e
Risa Takata <beat1422@icloud.com>
2020-01-16 22:49:39 -0800

    add some code  for modal ,but it's still not working well.

--- src/component/BookCard.jsx
+++ src/component/BookCard.jsx
@@ -1,17 +1,44 @@
 import React from "react"
+import Modal from "./Modal"
 
-const BookCard = (props) => {
-    const { image ,title,author,published} = props;
-    return (
+class BookCard extends React.Component {
+    // const {image ,title,author,published} = this.props;
+    constructor(props){
+    super(props)
+    this.state = { show: false }
+    }
+    
+  
+    showModal = () => {
+      this.setState({ show: true });
+    }
+    
+    hideModal = () => {
+      this.setState({ show: false });
+    }
+
+    render(){
+     return (
         <div className="card_container">
-            <img src={image} alt="" />
+            <img src={this.props.image} alt="" />
             <div className="desc">
-                <h2>{title}</h2>
-                <h3>{author}</h3>
-                <p>{published}</p>
+                <h2>{this.props.title}</h2>
+                <h3>{this.props.author}</h3>
+                <p>{this.props.published}</p>
+                
+
+                <main>
+          <h1>React Modal</h1>
+          <Modal show={this.state.show} handleClose={this.hideModal} >
+            <p>Modal</p>
+            <p>Data</p>
+          </Modal>
+          <button type='button' onClick={this.showModal}>Open</button>
+        </main>
             </div>
         </div>
     )
+}
 }
 
 export default BookCard;
