bdc4aa4502d0bb53213681528b051783e0bf1ce8
Risa Takata <beat1422@icloud.com>
2020-01-23 18:08:12 -0800

    just added react router

--- src/component/BookCard.jsx
+++ src/component/BookCard.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Modal from "./Modal"
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 class BookCard extends React.Component {
@@ -28,16 +29,18 @@ class BookCard extends React.Component {
                 <h2>{this.props.title}</h2>
                 <h3>{this.props.author}</h3>
                 <p>{this.props.published}</p>
-            <div className="modal_area">
-                <Modal show={this.state.show} handleClose={this.hideModal} >
-                   <img src={this.props.image} alt="" />
-                    <div className="desc">
-                    <h2>{this.props.title}</h2>
-                    <h3>{this.props.author}</h3>
-                    <p>{this.props.published}</p>
-                    <p>{this.props.description}</p>
-                    </div>
-                </Modal>
+                 <div className="modal_area">
+                <Router>
+                    <Modal show={this.state.show} handleClose={this.hideModal} >
+                        <img src={this.props.image} alt="" />
+                        <div className="desc">
+                            <h2>{this.props.title}</h2>
+                            <h3>{this.props.author}</h3>
+                            <p>{this.props.published}</p>
+                            <p>{this.props.description}</p>
+                        </div>
+                    </Modal>
+                </Router>
                 <button type='button' onClick={this.showModal}>Open</button>
             </div>
             </div>
--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 import "./Modal.scss"
+import {Link,Route} from "react-router-dom"
   
   const Modal = ({ handleClose, show ,children}) => {
      const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+  
+    const WannaRead = ()=> <h1>wannaread booklist</h1>
+    const AlreadyRead= ()=> <h1>alreadyread booklist</h1>
   
     return (
       <div className={showHideClassName}>
@@ -13,6 +17,11 @@ import "./Modal.scss"
           >
             Close
           </button>
+          <Link to="/wannaRead">wannaRead</Link>
+          <Link to="/alreadyRead">alreadyRead</Link>
+          <Route exact path="/wannaRead" component={WannaRead} />
+          <Route exact path="/alreadyRead" component={AlreadyRead} />
+
         </section>
       </div>
     );
